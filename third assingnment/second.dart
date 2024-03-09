40e165a1e9481655403c52bee399cd0b34734595
sheheryarhilal1 <162013692+sheheryarhilal1@users.noreply.github.com>
2024-03-10 03:33:03 +0500

    Add files via upload

---
+++ third assingnment/second.dart
@@ -0,0 +1,13 @@
+void main(){
+int age = 40;
+if (age < 20){
+
+  print("Your are Youngest");
+}
+else{
+
+  print("Your are oldest");
+}
+
+
+}
---
+++ third assingnment/third.dart
@@ -0,0 +1,18 @@
+void main(){
+  int number = 5;
+  if(number < 0){
+print("The numnber is negative");
+
+}
+else if(number > 0){
+
+  print("The number is positive");
+}
+
+else if(number == 0){
+
+  print("The number is equal to 0");
+}
+
+
+}
